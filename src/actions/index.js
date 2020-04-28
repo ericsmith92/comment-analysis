755ef165e170bf78a9af107bf532d8998b5e2118
ericsmith92 <ericsmth884@gmail.com>
2020-04-28 08:21:08 -0400

    recursively fetch comments until we have all

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,9 +2,22 @@ import youtube from '../apis/youtube';
 
 //we will need to pass the ID here
 
-export const fetchComments = id => async dispatch=> {
-    const response = await youtube.get(`/commentThreads?part=snippet%2Creplies&videoId=${id}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&maxResults=100`);
+export const fetchComments = id => async (dispatch)=> {
+    const comments = await performFetch(id);
+    dispatch({ type: 'FETCH_COMMENTS', payload: comments });
+}
 
-    dispatch({ type: 'FETCH_COMMENTS', payload: response.data.items });
+const performFetch = async (id) => {
+    const response = await youtube.get(`/commentThreads?part=snippet%2Creplies&videoId=${id}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&maxResults=100`);
+    const comments = response.data.items;
+    let nextPageToken = response.data.nextPageToken;
+
+    while(nextPageToken){
+        const recursiveResponse = await youtube.get(`/commentThreads?part=snippet%2Creplies&videoId=${id}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&maxResults=100&pageToken=${nextPageToken}`);
+        comments.push(recursiveResponse.data.items);
+        nextPageToken = recursiveResponse.data.nextPageToken;
+    }
+
+    return comments;
 }
 
